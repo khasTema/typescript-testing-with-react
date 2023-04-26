257af5c8eff368e2f8942dc0c8ef9cb6bb9b2966
Artem Khasin <91724948+khasTema@users.noreply.github.com>
2023-04-26 18:20:56 +0200

    list component | render cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
 import React, { useState } from 'react';
 import './App.css';
+import List from './components/List';
 
-// const people: {
-//   name: string;
-//   url: string;
-//   age: number;
-//   notes: string;
-// }[]
+interface IState {
+  people: {
+    map(arg0: (p: any) => any): unknown;
+    name: string,
+    age: number,
+    url: string,
+    note?: string | number
+  }[]
+   
+}
 
 function App() {
 
-  const [ people, setPeople ] = useState([])
+  const [people, setPeople] = useState<IState["people"]>([])
+
+  
 
 
   return (
     <div className="App">
       <h1>People invited to my Party</h1>
+      <List />
     </div>
   );
 }
